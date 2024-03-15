634ce11ef3d53b06c8b8e5b54a88a84a52d00563
Farkhadov95 <farkhadov950405@gmail.com>
2024-03-15 23:11:44 +0500

    Fixed Like Button

--- src/components/item-cards/ItemContainer.tsx
+++ src/components/item-cards/ItemContainer.tsx
@@ -9,25 +9,25 @@ import useErrorHandler from "../../hooks/useError";
 
 const ItemContainer = () => {
   const collectionID = useParams().id || "";
-  const userItems = useCollectionStore((state) => state.userItems);
-  const setUserItems = useCollectionStore((state) => state.setUserItems);
+  const items = useCollectionStore((state) => state.items);
+  const setItems = useCollectionStore((state) => state.setItems);
   const { handleFail } = useErrorHandler();
 
   useEffect(() => {
     getItems(collectionID)
       .then((res) => {
-        setUserItems(res);
+        setItems(res);
       })
       .catch((err) => {
         const errorMessage = err.message.toString();
         handleFail(errorMessage);
       });
-  }, [collectionID, handleFail, setUserItems]);
+  }, [collectionID, handleFail, setItems]);
 
   return (
     <Box marginTop={5}>
       <CollectionTools />
-      {userItems.length === 0 && (
+      {items.length === 0 && (
         <HStack justifyContent={"center"} paddingTop={"100px"}>
           <Heading fontSize={"large"}>
             No Items in this collection yet! Add some!
@@ -39,8 +39,7 @@ const ItemContainer = () => {
         columns={{ base: 1, sm: 2, md: 2, lg: 3, xl: 4 }}
         spacing={5}
       >
-        {userItems &&
-          userItems.map((item) => <ItemCard key={item._id} item={item} />)}
+        {items && items.map((item) => <ItemCard key={item._id} item={item} />)}
       </SimpleGrid>
     </Box>
   );

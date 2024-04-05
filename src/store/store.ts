b7a763ed586455012ed7f7f4a0659d9253a612c7
Farkhadov95 <farkhadov950405@gmail.com>
2024-04-05 13:47:52 +0500

    spacing fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -34,65 +34,65 @@ type CollectionStore = {
 }
 
 export const useCollectionStore = create<CollectionStore>()(
-    persist(
-      (set) => ({
-        currentUser: emptyCurrentUser,
-        collections: [],
-        biggestCollections: [],
-        items: [],
-        users: [],
-        userCollections: [],
-        userItems: [],
-        comments: [],
-        setCurrentUser: (user: currentUser) => set({ currentUser: user }),
-        setCollections: (collections: Collection[]) => set({ collections }),
-        setBiggestCollections: (biggestCollections: Collection[]) => set({ biggestCollections }),
-        setItems: (items: ItemType[]) => set({ items }),
-        setUsers: (users: userInfo[]) => set({ users }),
-        setUserCollections: (userCollections: Collection[]) => set({ userCollections }),
-        setUserItems: (userItems: ItemType[]) => set({ userItems }),
-        setComments: (comments: Comment[]) => set({ comments }),
-      }),
-      {
-        name: 'collections-storage',
-      }
-    )
-  );
+  persist(
+    (set) => ({
+      currentUser: emptyCurrentUser,
+      collections: [],
+      biggestCollections: [],
+      items: [],
+      users: [],
+      userCollections: [],
+      userItems: [],
+      comments: [],
+      setCurrentUser: (user: currentUser) => set({ currentUser: user }),
+      setCollections: (collections: Collection[]) => set({ collections }),
+      setBiggestCollections: (biggestCollections: Collection[]) => set({ biggestCollections }),
+      setItems: (items: ItemType[]) => set({ items }),
+      setUsers: (users: userInfo[]) => set({ users }),
+      setUserCollections: (userCollections: Collection[]) => set({ userCollections }),
+      setUserItems: (userItems: ItemType[]) => set({ userItems }),
+      setComments: (comments: Comment[]) => set({ comments }),
+    }),
+    {
+      name: 'collections-storage',
+    }
+  )
+);
 
-  type nonPersistStore = {
-    userError: string,
-    error: string,
-    commentLoading: boolean,
-    featuresLoading: boolean,
-    searchedItems: ItemSearch[],
-    searchedComments: CommentSearch[],
-    searchedCollections: CollectionSearch[],
-    setUserError: (userError: string) => void,
-    setError: (error: string) => void,
-    setCommentLoading: (loading: boolean) => void,
-    setFeaturesLoading: (loading: boolean) => void,
-    setSearchedItems: (searchedItems: ItemSearch[]) => void,
-    setSearchedComments: (searchedComments: CommentSearch[]) => void,
-    setSearchedCollections: (searchedCollections: CollectionSearch[]) => void,
-  }
+type nonPersistStore = {
+  userError: string,
+  error: string,
+  commentLoading: boolean,
+  featuresLoading: boolean,
+  searchedItems: ItemSearch[],
+  searchedComments: CommentSearch[],
+  searchedCollections: CollectionSearch[],
+  setUserError: (userError: string) => void,
+  setError: (error: string) => void,
+  setCommentLoading: (loading: boolean) => void,
+  setFeaturesLoading: (loading: boolean) => void,
+  setSearchedItems: (searchedItems: ItemSearch[]) => void,
+  setSearchedComments: (searchedComments: CommentSearch[]) => void,
+  setSearchedCollections: (searchedCollections: CollectionSearch[]) => void,
+}
 
-  export const useNonPersistStore = create<nonPersistStore>((set) => ({
-    userError: "",
-    error: "",
-    commentLoading: false,
-    featuresLoading: false,
-    searchedItems: [],
-    searchedComments: [],
-    searchedCollections: [],
-    setUserError: (userError: string) => set({ userError }),
-    setError: (error: string) => set({ error }),
-    setCommentLoading: (commentLoading: boolean) => set({ commentLoading }),
-    setFeaturesLoading: (featuresLoading: boolean) => set({ featuresLoading }),
-    setSearchedItems: (searchedItems: ItemSearch[]) => set({ searchedItems }),
-    setSearchedComments: (searchedComments: CommentSearch[]) => set({ searchedComments }),
-    setSearchedCollections: (searchedCollections: CollectionSearch[]) => set({ searchedCollections }),
-  }))
+export const useNonPersistStore = create<nonPersistStore>((set) => ({
+  userError: "",
+  error: "",
+  commentLoading: false,
+  featuresLoading: false,
+  searchedItems: [],
+  searchedComments: [],
+  searchedCollections: [],
+  setUserError: (userError: string) => set({ userError }),
+  setError: (error: string) => set({ error }),
+  setCommentLoading: (commentLoading: boolean) => set({ commentLoading }),
+  setFeaturesLoading: (featuresLoading: boolean) => set({ featuresLoading }),
+  setSearchedItems: (searchedItems: ItemSearch[]) => set({ searchedItems }),
+  setSearchedComments: (searchedComments: CommentSearch[]) => set({ searchedComments }),
+  setSearchedCollections: (searchedCollections: CollectionSearch[]) => set({ searchedCollections }),
+}))
 
-  if (process.env.NODE_ENV === "development") {
-    mountStoreDevtool("CollectionStore", useCollectionStore);
-  }
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("CollectionStore", useCollectionStore);
+}
